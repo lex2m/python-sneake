1130a5f2b22241ea6cf1265b2d995867827f85d3
Lex2m <87268926+lex2m@users.noreply.github.com>
2023-03-22 14:47:56 +0300

    Add files via upload

---
+++ yilan.py
@@ -0,0 +1,148 @@
+import random
+import turtle
+import time
+delay = 0.15
+
+    # Uygulama penceresi.
+pencere = turtle.Screen()
+pencere.title('Yılan Oyunu')
+pencere.bgcolor('lightgreen')
+pencere.setup(width=600, height=600)
+pencere.tracer(0)
+
+    # Yılanın kafası (Pembe kare).
+kafa = turtle.Turtle()
+kafa.speed(0)
+kafa.shape("square")
+kafa.color("#f5aaf5")
+kafa.penup()
+kafa.goto(0, 100)
+kafa.direction = "stop"
+
+    # Yemek (Kırmızı Yuvarlak).
+yemek = turtle.Turtle()
+yemek.speed(0)
+yemek.shape("circle")
+yemek.color("red")
+yemek.penup()
+yemek.shapesize(0.90, 0.90)
+yemek.goto(0, 0)
+
+kuyruklar = []  # Kuyruk dizisi.
+puan = 0        # Puan durumu.
+
+    # Puan tablosu.
+yaz = turtle.Turtle()
+yaz.speed(0)
+yaz.shape("square")
+yaz.color("black")
+yaz.penup()
+yaz.hideturtle()
+yaz.goto(0, 260)
+yaz.write("Puan: {}".format(puan), align="center", font=("Courier", 24, "normal"))
+
+    # Hareket kodları.
+def move():
+    if kafa.direction == "up":
+        y = kafa.ycor()
+        kafa.sety(y + 20)
+    if kafa.direction == "down":
+        y = kafa.ycor()
+        kafa.sety(y - 20)
+    if kafa.direction == "right":
+        x = kafa.xcor()
+        kafa.setx(x + 20)
+    if kafa.direction == "left":
+        x = kafa.xcor()
+        kafa.setx(x - 20)
+
+def go_up():
+    if kafa.direction != "down":
+        kafa.direction = "up"
+def go_down():
+    if kafa.direction != "up":
+        kafa.direction = "down"
+
+
+
+
+
+def go_right():
+    if kafa.direction != "left":
+        kafa.direction = "right"
+def go_left():
+    if kafa.direction != "right":
+        kafa.direction = "left"
+
+    # Uygulamaya tuş basımını gönderme.
+pencere.listen()
+pencere.onkey(go_up, "Up")
+pencere.onkey(go_down, "Down")
+pencere.onkey(go_right, "Right")
+pencere.onkey(go_left, "Left")
+
+while True:
+    pencere.update() # while döngüsü bitmeden uygulama kapandığı için bazen hata veriyormuş gibi gösteriyor.
+
+    if kafa.xcor() > 300 or kafa.xcor() < -300 or kafa.ycor() > 300 or kafa.ycor() < -300:
+        time.sleep(1)
+        kafa.goto(0, 0)
+        kafa.direction = "stop"
+
+        for kuyruk in kuyruklar:
+            kuyruk.goto(1000, 1000)
+        kuyruklar = []
+
+        puan = 0
+        delay = 0.1
+
+        yaz.clear()
+        yaz.write("Puan: {}".format(puan), align="center", font=("Courier", 24, "normal"))
+
+        # Kafa yemeğe değince kuyruk ekleme.
+    if kafa.distance(yemek) < 20:
+        x = random.randint(-250, 250)
+        y = random.randint(-250, 250)
+        yemek.goto(x, y)
+
+        # Kuyruk kodları (beyaz kare).
+
+        yeni_kuyruk = turtle.Turtle()
+        yeni_kuyruk.speed(0)
+        yeni_kuyruk.shape("square")
+        yeni_kuyruk.color("white")
+        yeni_kuyruk.penup()
+        kuyruklar.append(yeni_kuyruk)
+
+        delay -= 0.001
+
+        puan = puan + 10
+        yaz.clear()
+        yaz.write("Puan: {}".format(puan), align="center", font=("Courier", 24, "normal"))
+
+    for index in range(len(kuyruklar) - 1, 0, -1):
+        x = kuyruklar[index - 1].xcor()
+        y = kuyruklar[index - 1].ycor()
+        kuyruklar[index].goto(x, y)
+
+    if len(kuyruklar) > 0:
+        x = kafa.xcor()
+        y = kafa.ycor()
+        kuyruklar[0].goto(x, y)
+
+    move()
+
+    for segment in kuyruklar:
+        if segment.distance(kafa) < 20:
+            time.sleep(1)
+            kafa.goto(0, 0)
+            kafa.direction = "stop"
+            for segment in kuyruklar:
+                segment.goto(1000, 1000)
+            kuyruklar = []
+            puan = 0
+            yaz.clear()
+            yaz.write('Puan: {}'.format(puan), align='center', font=('Courier', 24, 'normal'))
+            hiz = 0.15
+
+    time.sleep(delay)
